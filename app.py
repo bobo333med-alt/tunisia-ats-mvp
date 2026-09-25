47178131612a61c19bf174ec3c4c0cca33c61b9e
bobo333med-alt <bobo333med@gmail.com>
2026-09-25 17:34:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -131,7 +131,7 @@ def startup():
 
 
 # ---------- Health ----------
-@app.get('/health')
+@app.api_route('/health', methods=['GET', 'HEAD'])
 def health():
     return {'status': 'ok', 'service': 'Tunisia ATS', 'version': '1.0'}
 
